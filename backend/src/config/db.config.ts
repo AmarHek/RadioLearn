82214507e52c91ee429ebcc4a069e558c8a67ce3
Amar Hekalo <amar.hekalo@uni-wuerzburg.de>
2024-03-22 18:54:43 +0100

    update dbConfig to adapt to docker and dev/prod envs

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,5 +1,31 @@
-export const dbConfig = {
+interface DataBaseConfig {
+    HOST: string;
+    PORT: number;
+    DB: string;
+}
+
+// Configuration for development and production environment
+// (i.e. mongoDB running on local machine)
+const localConfig: DataBaseConfig = {
     HOST: "127.0.0.1",
     PORT: 27017,
     DB: "radiolearn"
 };
+
+// Configuration for running in a docker-compose environment
+const dockerConfig: DataBaseConfig = {
+    HOST: "radiolearn-mongodb",
+    PORT: 27017,
+    DB: "radiolearn"
+};
+
+// Determine environment based on NODE_ENV or default to development
+const environment = process.env.NODE_ENV || 'development';
+
+export let dbConfig: DataBaseConfig;
+
+if (environment === 'docker') {
+    dbConfig = dockerConfig;
+} else {
+    dbConfig = localConfig;
+}
