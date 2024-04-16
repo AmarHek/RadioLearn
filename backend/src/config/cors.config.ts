4192dfda2d390935749b2abbbeb23c67ed0a4455
AmarHek <amar.hekalo@uni-wuerzburg.de>
2024-04-16 13:10:20 +0200

    update cors config

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -1,2 +1,2 @@
-export const allowedOrigins = ["http://localhost:4200", "https://radiospeech.informatik.uni-wuerzburg.de"]
+export const allowedOrigins = ["http://localhost:4200", "https://radiolearn.informatik.uni-wuerzburg.de"]
 
