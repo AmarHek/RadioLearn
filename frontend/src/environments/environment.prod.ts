5e626570fd9d886ebd3212692fc28ccd85d2a8b5
AmarHek <amar.hekalo@uni-wuerzburg.de>
2024-04-16 13:10:12 +0200

    update environment config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  backend: "https://radiospeech.informatik.uni-wuerzburg.de/",
-  authentication: "https://radiospeech.informatik.uni-wuerzburg.de/auth/",
-  database: "https://radiospeech.informatik.uni-wuerzburg.de/database/",
-  images: "https://radiospeech.informatik.uni-wuerzburg.de/images/",
-  assets: "https://radiospeech.informatik.uni-wuerzburg.de/assets/"
+  backend: "https://radiolearn.informatik.uni-wuerzburg.de/",
+  authentication: "https://radiolearn.informatik.uni-wuerzburg.de/auth/",
+  database: "https://radiolearn.informatik.uni-wuerzburg.de/database/",
+  images: "https://radiolearn.informatik.uni-wuerzburg.de/images/",
+  assets: "https://radiolearn.informatik.uni-wuerzburg.de/assets/"
 };
