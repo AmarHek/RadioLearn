4c77fcd08943614ec2824aaa106cb8c90739c89a
Amar Hekalo <amar.hekalo@uni-wuerzburg.de>
2024-04-17 18:57:50 +0200

    fixed docker filepaths

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -36,7 +36,7 @@ app.use(bodyParser.urlencoded({
 app.use("/images", express.static(path.join(dataPathConfig.path, "images")));
 
 if (process.env.NODE_ENV === "docker") {
-    app.use("/", express.static(path.join(__dirname, "..", "dist", "public", "radiolearn")));
+    app.use("/", express.static(path.join(__dirname, "..", "dist", "radiolearn")));
 } else {
     app.use("/radiolearn", express.static(path.join(__dirname, "..", "dist", "radiolearn")));
 }
